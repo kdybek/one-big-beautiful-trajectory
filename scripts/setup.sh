2760d951507a589a95780f18850586ae6098c8c4
kdybek <kam.dyb1@gmail.com>
2025-12-04 19:38:40 +0100

    made setup script great again

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,9 +3,10 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
+#SBATCH --gres=gpu:1
 #SBATCH --time=1:00:00
-#SBATCH --account=plgcrlreason-cpu
-#SBATCH --partition=plgrid
+#SBATCH --account=plgcrlreason-gpu-gh200
+#SBATCH --partition=plgrid-gpu-gh200
 #SBATCH --output=setup_out.txt
 
 ml ML-bundle/24.06a
