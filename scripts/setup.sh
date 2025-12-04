9f4b96da8478ae80970384878b6d15f5937e5bf2
kdybek <kam.dyb1@gmail.com>
2025-12-04 19:15:15 +0100

    made setup script great again

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --gres=gpu:1
 #SBATCH --time=1:00:00
 #SBATCH --account=plgcrlreason-gpu-gh200
